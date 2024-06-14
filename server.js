ba3906af5e4ae5b86ecd31a1af0eb47f9c403adc
Pattanan <pattanan@Ampere-2.local>
2024-06-14 12:30:25 +0700

    add file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000, () => {
+  console.log('Start server at port 3000.')
+})
+
+const books = require('./db')
+
+app.get('/books', (req, res) => {
+  res.json(books)
+})
+
+app.post('/books', (req, res) => {
+    books.push(req.body)
+    res.status(201).json(req.body)
+  })
+
